ab3221ec13d59afacf21806c4885ebc06bdd6a71
Mhmdaki102 <mhmdali102@bk.ru>
2021-02-21 23:54:56 +0200

    adding age calculator

--- Youtube-info/youtube_info.py
+++ Youtube-info/youtube_info.py
@@ -1,10 +1,10 @@
 from apiclient.discovery import build
 
 #-----API-KEY-----#
-key = 'Your Key'
+key = 'AIzaSyB9wA_ywK-xsBl-shsSFbRgQFdu_JifZYE'
 youtube = build('youtube', 'v3', developerKey= key)
-res = youtube.channels().list(id=UCMbleSlNSOJZvWF3tqY5bCw, part=statistics).execute()
-print(ANDRO TOP YouTube Channel)
-print(res['items'][0]['statistics']['subscriberCount'] + Subscribe)
-print(res['items'][0]['statistics']['viewCount'] + Views)
-print(res['items'][0]['statistics']['videoCount'] + Video)
+res = youtube.channels().list(id="UCMbleSlNSOJZvWF3tqY5bCw", part="statistics").execute()
+print("ANDRO TOP YouTube Channel")
+print(res['items'][0]['statistics']['subscriberCount'] + " Subscribe")
+print(res['items'][0]['statistics']['viewCount'] + " Views")
+print(res['items'][0]['statistics']['videoCount'] + " Video")
