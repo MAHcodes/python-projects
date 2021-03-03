d7efb27e25d9fc9620ad03dfed4b618bc7767606
mhmdali102 <mhmdali102@bk.ru>
2021-03-03 18:19:30 +0200

    some fixes

---
+++ Rename-multiple-files/README.md
@@ -0,0 +1,16 @@
+# Rename Multiple Files
+ A simple script to rename multiple files in a selected folder.
+
+## Prerequisites
+You only need Python to run this script. You can visit [here](https://www.python.org/downloads/) to download Python.  
+**Or** you can simply use any Python online compiler.
+
+## How to run it?
+* Running the script is really simple! Just open a terminal in the folder where your script is located and run the following command :
+
+```
+python3 rename_multiple_files.py
+```
+
+## Author Name
+[Mhmd Ali Hsen](https://github.com/mhmdali102)
--- Rename-multiple-files/rename_multiple_files.py
+++ Rename-multiple-files/rename_multiple_files.py
@@ -4,14 +4,16 @@ import os
 def main():
 	counter = 1
 	for file in os.listdir(path + "\\"):
-	    file_extension = os.path.splitext(file)[1]
-	    new_name = "mah_" + str(counter) + file_extension
-	    source = path + "\\" + file
-	    destination = path + "\\" + new_name
-	    os.rename(source, destination)
-	    counter += 1
+		file_extension = os.path.splitext(file)[1]
+		new_name = name  + str(counter) + file_extension
+		source = path + "\\" + file
+		destination = path + "\\" + new_name
+		os.rename(source, destination)
+		counter += 1
 	print("All files renamed successfully...")
 
 
 if __name__ == '__main__':
+	path = input("Enter folder path: ")
+	name = input("Enter files name: ")
 	main()
