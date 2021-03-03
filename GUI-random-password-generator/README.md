ce0336c5c1376e98e1547c0432a56022070a35bc
mhmdali102 <mhmdali102@bk.ru>
2021-03-03 19:53:27 +0200

    added copy to clipboard feature

--- GUI-random-password-generator/README.md
+++ GUI-random-password-generator/README.md
@@ -3,6 +3,7 @@ A simple GUI tool to generate random passwords.
 * Generate a random password with different strength.
 * Generate a random password with custom length.
 * Generate a non duplicate characters.
+* Copy your generated password with one click
   
 ## Prerequisites
 You only need Python to run this script. You can visit [here](https://www.python.org/downloads/) to download Python.  
@@ -12,19 +13,11 @@ You only need Python to run this script. You can visit [here](https://www.python
 * Running the app is really simple! Just open a terminal in the folder where your script is located and run the following command :
 
 ```
-python password_generator.py
+python3 password_generator.py
 ```
 
-**Or**  
-
-* Install and open the `.exe` file
-
-> Note: Disable real-time protection to be able to run the app.
-### How to turn off real-time protection?
-*Follow this path: (Windows 10)*  
-Settings/ Update and security/ Windows Security/ Virus and thread protection/ Manage settings/ Turn off
-  
 ## Screenshot showing the sample use of the app
 ![](screenshot.png)  
+
 ## Author Name
 [Mhmd Ali Hsen](https://github.com/mhmdali102)
--- GUI-random-password-generator/password_generator.py
+++ GUI-random-password-generator/password_generator.py
@@ -1,10 +1,11 @@
 import random
 from tkinter import *
 from tkinter import messagebox
+import subprocess
 
 root = Tk()
 root.title("Random Password Generator")
-root.geometry("200x380+400+100")
+root.geometry("230x440+400+100")
 root.resizable(False, False)
 var = IntVar()
 chVar1 = BooleanVar()
@@ -20,10 +21,11 @@ pwd = ""
 upper_case = "ABCDEFGHIJKLMNOPQRSTUVWYXZ"
 lower_case = upper_case.lower()
 num = "0123456789"
-symbols = "!@#$%^&*~?/"
+symbols = "!@#$%^=*~?/"
 
 
 def generate():
+    lbl4.config(text="")
     chosen = ''
     if chVar1.get() == False and chVar2.get() == False and chVar3.get() == False and chVar4.get() == False:
         messagebox.showwarning("Error", "Please check at least one")
@@ -48,8 +50,17 @@ def generate():
 def check():
     if (chVar1.get() == False and chVar2.get() == False and chVar3.get() == False and chVar4.get()) or (chVar1.get() == False and chVar2.get() == False and chVar4.get() == False and chVar3.get()):
         scale.config(to=10, tickinterval=1)
-    else:
+    elif (chVar1.get() == False and chVar2.get() == False and chVar4.get() and chVar3.get()):
         scale.config(to=20, tickinterval=2)
+    else:
+        scale.config(to=24, tickinterval=2)
+
+
+def copy2clip():
+    text = txt.get(1.0, "end-1c")
+    cmd='echo '+text.strip()+'|clip'
+    lbl4.config(text="Copied to your clipboard!")
+    return subprocess.check_call(cmd, shell=True)
 
 
 label1 = Label(root, text="Password strength:").pack(pady=5)
@@ -57,10 +68,13 @@ check1 = Checkbutton(root, text="Uppercase", onvalue=1, offvalue=0, variable=chV
 check2 = Checkbutton(root, text="Lowercase", onvalue=1, offvalue=0, variable=chVar2, command=check).pack()
 check3 = Checkbutton(root, text="Numbers", onvalue=1, offvalue=0, variable=chVar3, command=check).pack()
 check4 = Checkbutton(root, text="Symbols", onvalue=1, offvalue=0, variable=chVar4, command=check).pack()
-scale = Scale(root, orient=HORIZONTAL, length=150, label="Password length", from_=8, to=20, width=10, tickinterval=2, variable=var)
+scale = Scale(root, orient=HORIZONTAL, length=200, label="Password length", from_=8, to=24, width=12, tickinterval=2, variable=var)
 scale.pack(pady=20)
 lbl3 = Label(root, text="Your random password is: ").pack(pady=20)
-txt = Text(root, height=1, width=22)
+txt = Text(root, height=1, width=26)
 txt.pack()
+lbl4 = Label(root, text="")
+lbl4.pack()
 btn = Button(root, text="Generate", command=generate).pack(pady=10)
+btn2 = Button(root, text="Copy", command=copy2clip).pack(pady=5)
 root.mainloop()
