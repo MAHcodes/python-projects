e7a938f5de011acde4a8251b6a046abec0615e6c
mhmdali102 <mhmdali102@bk.ru>
2021-03-08 20:22:28 +0200

    weather scraper

---
+++ Weather-scraper/README.md
@@ -0,0 +1,27 @@
+# Weather scraper
+Script to get the weather status.
+* Get the current weather status with the temperature for your city automatically.
+* Get the weather hourly forecast for your city.
+* Get the weather daily forecast for your city. 
+
+## Prerequisites
+You need Python 3.X and some modules installed in your machine to run this script.
+* To download Python, you can visit [here](https://www.python.org/downloads/). 
+* You can easly download the `requirements.txt` file and run the following command in a terminal :
+  ```pip3 install -r requirements.txt```
+
+**Or** you can simply use any Python online compiler.
+
+## How to run the script?
+
+* Running the script is really simple! Just open a terminal in the folder where your script is located and run the following command :
+
+    ```
+    python3 weather_app.py
+    ```  
+
+## Screenshot showing the sample use of the script
+![](screenshot.png)  
+
+## Author Name
+[Mhmd Ali Hsen](https://github.com/mhmdali102)
--- Weather-scraper/weather-scraper.py
+++ Weather-scraper/weather-scraper.py
@@ -3,42 +3,29 @@ import requests
 
 r = requests.get("https://weather.com/weather/today/l/33.24,35.38?par=google&temp=c").text
 soup = BeautifulSoup(r, "lxml")
-
 location = soup.find("h1", class_="CurrentConditions--location--1Ayv3").text
 time = soup.find("div", class_="CurrentConditions--timestamp--1SWy5").text
 temp = soup.find("span", class_="CurrentConditions--tempValue--3KcTQ").text
 status = soup.find("div", class_="CurrentConditions--phraseValue--2xXSr").text
 time = time.split()[2:4]
 
-def now_temp():
-    print(f"{location}\n{time[0]}{time[1]}\nTemperature: {temp} ({status})")
-
-def today_temp():
-    sec = soup.find("section", class_="card Card--card--4VS_Q")
-    print(f"{location} | {time[0]} {time[1]}")
+def main(forecast):
+    sec = soup.find("section", title=forecast)
+    print(f"\n{location} | {time[0]} {time[1]}")
     for i in sec.find_all("li", class_="Column--column--2bRa6"):
         today = i.find("h3", class_="Column--label--L3RrD Column--default--2gARi").text
         today_temp = i.find("div", class_="Column--temp--2v_go").text
-        divtitle = sec.find("div", class_="Column--icon--1fMZT")
+        divtitle = i.find("div", class_="Column--icon--1fMZT")
         title = divtitle.find("title").text
+        if today_temp == "--" and today == "Today":
+            today_temp = temp
         print(f"{today}: {today_temp} ({title})")
 
-def hourly_temp():
-    sec = soup.find("section", title="Hourly Forecast")
-    print(f"{location} | {time[0]} {time[1]}")
-    for i in sec.find_all("li", class_="Column--column--2bRa6"):
-        today = i.find("h3", class_="Column--label--L3RrD Column--default--2gARi").text
-        today_temp = i.find("div", class_="Column--temp--2v_go").text
-        divtitle = sec.find("div", class_="Column--icon--1fMZT")
-        title = divtitle.find("title").text
-        print(f"{today}: {today_temp} ({title})")
 
-def daily_tmep():
-    sec = soup.find("section", title="Daily Forecast")
-    print(f"{location} | {time[0]} {time[1]}")
-    for i in sec.find_all("li", class_="Column--column--2bRa6"):
-        today = i.find("h3", class_="Column--label--L3RrD Column--default--2gARi").text
-        today_temp = i.find("div", class_="Column--temp--2v_go").text
-        divtitle = sec.find("div", class_="Column--icon--1fMZT")
-        title = divtitle.find("title").text
-        print(f"{today}: {today_temp} ({title})")
+if __name__ == "__main__":
+    user = int(input(f"{location} | {time[0]} {time[1]}\nCurrent weather: {temp} ({status})\n\n1. Hourly Forecast\n2. Daily Forecast\nChoose number to get more info: "))
+    if user == 1:
+        forecast = "Hourly Forecast"
+    elif user == 2:
+        forecast = "Daily Forecast"
+    main(forecast)
